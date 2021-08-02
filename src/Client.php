18fffe12877d14c69ef5a3b2b624150b13c8efe2
garam <garam-park@naver.com>
2021-08-02 10:48:08 +0900

    API Credits API  #2 작성 완료

--- src/Client.php
+++ src/Client.php
@@ -3,6 +3,7 @@
 namespace Ntuple\MillionverifierClient;
 
 use Ntuple\MillionverifierClient\Exceptions\EmailsRequiredException;
+use Ntuple\MillionverifierClient\CheckCredits\Response as CheckCreditsResponse;
 use Ntuple\MillionverifierClient\VerifyEmail\Request as VerifyEmailRequest;
 use Ntuple\MillionverifierClient\VerifyEmail\Response as VerifyEmailResponse;
 
@@ -44,5 +45,27 @@ class Client
 
         $array = json_decode($response, true);
         return VerifyEmailResponse::fromArray($array);
+    }
+
+    public function checkCredits()
+    {
+        $curl = curl_init();
+        $api_key = $this->api_key;
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.millionverifier.com/api/v3/credits?api=$api_key",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $array = json_decode($response, true);
+        return CheckCreditsResponse::fromArray($array);
     }
 }
